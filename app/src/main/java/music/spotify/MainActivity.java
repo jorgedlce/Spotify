6fddb4df17b4a5542923927a8ed48e47caf5fb6c
jorge <jorgeluisdc_07@hotmailc.om>
2016-01-11 10:25:48 -0500

    primera fase

--- app/src/main/java/music/spotify/MainActivity.java
+++ app/src/main/java/music/spotify/MainActivity.java
@@ -1,7 +1,10 @@
 package music.spotify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Onclick(View v){
+
+        //Toast.makeText(this,"Holaa",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this,MyMusic.class);
+        startActivity(intent);
+
     }
 }
